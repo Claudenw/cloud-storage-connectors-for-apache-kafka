7a9a7c49cb88f8dafe95c99957c040e67d7281d3
¨Claude <claude.warren@aiven.io>
2025-02-12 14:41:39 +0000

    updataded doc generation

--- s3-source-connector/src/test/java/io/aiven/kafka/connect/s3/source/docs/S3SourceConfigDoc.java
+++ s3-source-connector/src/test/java/io/aiven/kafka/connect/s3/source/docs/S3SourceConfigDoc.java
@@ -1,4 +1,15 @@
 package io.aiven.kafka.connect.s3.source.docs;
 
+import io.aiven.kafka.connect.docs.ConfigDocumentation;
+import io.aiven.kafka.connect.s3.source.config.S3SourceConfig;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 public class S3SourceConfigDoc {
+    @Test
+    public void yaml() throws IOException {
+        ConfigDocumentation.main(new String[]{"-c", S3SourceConfig.class.getName(), "-f", "YAML"});
+
+    }
 }
