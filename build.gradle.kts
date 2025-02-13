5f19fb3495a4816e7788bfc682148d0bf973ee0d
¨Claude <claude.warren@aiven.io>
2025-02-13 10:31:54 +0000

    Attempts at fixing text issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ tasks.register<Exec>("execVale") {
     description = "Executes the Vale text linter"
     group = "Documentation"
     executable("/usr/bin/docker")
-    args("run", "--rm", "-v", "${project.rootDir}:/project:Z", "-v", "${project.rootDir}/.github/vale/styles:/styles:Z", "-v", "${project.projectDir}:/docs:Z", "-w", "/docs", "jdkato/vale", "--filter=warn.expr", "--config=/project/.vale.ini", "--glob=!**build**", ".")
+    args("run", "--rm", "-v", "${project.rootDir}:/project:Z", "-v", "${project.rootDir}/.github/vale/styles:/styles:Z", "-v", "${project.projectDir}:/docs:Z", "-w", "/docs", "jdkato/vale", "--filter=warn.expr", "--config=/project/.vale.ini", "--glob=!**/build/**", ".")
 }
 
 
